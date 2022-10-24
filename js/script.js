cc72ebe7859c0f158220bcc79dc8d17e0151952d
SimoCrash <simone.crescenzi@hotmail.com>
2022-10-24 18:13:55 +0200

    esercizio + BONUS 1 + BONUS 2

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@ const squares = document.querySelector(".squares");
 for (let i = 1; i <= 100; i++) {
     if ((i % (3 * 5)) == 0){
         console.log("fizzbuzz");
-        // squares.innerHTML += `<div class="fizzbuzz"> fizzbuzz </div>`;
+        squares.innerHTML += `<div class="fizzbuzz"> fizzbuzz </div>`;
     } else if ((i % 5) == 0) {
         console.log("buzz");
-        // squares.innerHTML += `<div class="buzz"> buzz </div>`
+        squares.innerHTML += `<div class="buzz"> buzz </div>`
     } else if ((i % 3) == 0){
         console.log("fizz");
-        // squares.innerHTML += `<div class="fizz"> fizz </div>`;
+        squares.innerHTML += `<div class="fizz"> fizz </div>`;
     } else {
         console.log(i);
-        // squares.innerHTML += `<div> ${i} </div>`;
+        squares.innerHTML += `<div> ${i} </div>`;
     }
 }
