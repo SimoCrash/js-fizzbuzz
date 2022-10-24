878ff06aa73e1a5421c6402140e3e79b249736e8
SimoCrash <simone.crescenzi@hotmail.com>
2022-10-24 18:08:38 +0200

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -2,12 +2,16 @@ const squares = document.querySelector(".squares");
 
 for (let i = 1; i <= 100; i++) {
     if ((i % (3 * 5)) == 0){
-        squares.innerHTML += `<div class="fizzbuzz"> fizzbuzz </div>`;
+        console.log("fizzbuzz");
+        // squares.innerHTML += `<div class="fizzbuzz"> fizzbuzz </div>`;
     } else if ((i % 5) == 0) {
-        squares.innerHTML += `<div class="buzz"> buzz </div>`
+        console.log("buzz");
+        // squares.innerHTML += `<div class="buzz"> buzz </div>`
     } else if ((i % 3) == 0){
-        squares.innerHTML += `<div class="fizz"> fizz </div>`;
+        console.log("fizz");
+        // squares.innerHTML += `<div class="fizz"> fizz </div>`;
     } else {
-        squares.innerHTML += `<div class="number"> ${i} </div>`;
+        console.log(i);
+        // squares.innerHTML += `<div> ${i} </div>`;
     }
 }
